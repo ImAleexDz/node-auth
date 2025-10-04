1ddbf242cb5f3a25ee70e9357372bb5e66769edb
Alejandro Diaz Alvarado <alejandrodiazalvarado@Alejandros-MacBook-Air.local>
2025-10-03 21:43:14 -0600

    create login method using JWT

---
+++ controllers/loginController.js
@@ -0,0 +1,37 @@
+
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ where: { email } });
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+        if (!user.isConfirmed) {
+            return res.status(403).json({ message: 'Account not confirmed. Please check your email.' });
+        }
+        // Create JWT token
+        const token = jwt.sign(
+            { id: user.id, email: user.email },
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' }
+        );
+        user.password = undefined;
+        userInfo = {
+            id: user.id,
+            email: user.email,
+            username: user.username,
+        }
+        res.json({ message: 'Login successful', userObject, token });
+    } catch (error) {
+        console.error('Error logging in:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+
 const express = require('express');
 const app = express();
 const port = 3000;
 const sequelize = require('./config/db');
 require('./models/user');
+
+const session = require('express-session');
+// const passport = require('passport');
+// require('./config/passport');
+
 
 sequelize.sync()
   .then(() => {
@@ -14,12 +20,22 @@ sequelize.sync()
     console.error('Unable to connect to the database:', err);
   });
 
+
 // Middleware to parse JSON
 app.use(express.json());
+
+// Session and Passport middleware
+app.use(session({ secret: process.env.JWT_SECRET, resave: false, saveUninitialized: true }));
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 // Auth routes
 const authRoutes = require('./routes/auth');
 app.use('/api/auth', authRoutes);
+
+// Login routes
+const loginRoutes = require('./routes/login');
+app.use('/api/login', loginRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
